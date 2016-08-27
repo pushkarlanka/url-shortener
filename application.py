d7589c414a449fc64ce65c295f4d448572a1808d
Pushkar Lanka <pushkar.lanka@gmail.com>
2016-08-26 20:32:24 -0500

    flashing hashed short url

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash
 from hash_url import UrlHash
+
 
 app = Flask(__name__)
 
@@ -12,6 +13,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = config.get_db_url()
 app.debug = True
 db = SQLAlchemy(app)
 
+# defined by me
+app.domain = None
 
 class UrlDB(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
@@ -27,9 +30,9 @@ class UrlDB(db.Model):
 @app.route('/')
 def index():
     db.create_all()
-
+    if app.domain is None:
+        app.domain = request.url
     # return redirect("http://www.google.com", code=302)
-
     # return render_template('add_link.html', name=name)
     return render_template('add_link.html')
 
@@ -47,8 +50,9 @@ def post_link():
     short_url = UrlHash.get_base_k(db_id)
     print 'short_url', short_url
 
-    return redirect(url_for('display_result', link=short_url))
-    # return render_template('add_link.html', short_url=short_url)
+    # return redirect(url_for('display_result', link=short_url))
+    flash(app.domain + short_url)
+    return redirect(url_for('index'))
 
 
 @app.route('/result')
@@ -56,5 +60,15 @@ def display_result():
     return render_template('add_link.html', short_url=request.args['link'])
     # return render_template('add_link.html', short_url=res)
 
+
+@app.route('/<short_url>')
+def short_to_long_url(short_url):
+    db_id = UrlHash.get_base_10(short_url)
+    obj = UrlDB.query.get(db_id)
+    print obj.long_url
+    return redirect("http://" + obj.long_url, code=302)
+
 if __name__ == '__main__':
+    app.secret_key = 'private key'
+
     app.run()
